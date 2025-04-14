c16004c2e8bc8491075b47616e3fdb5be228c8b6
Vtrech <vdtruong1501@gmail.com>
2025-04-15 00:25:35 +0700

    chatbot

--- config/gptConfig.js
+++
@@ -1,2 +0,0 @@
-// gptConfig.js
-export const OPENAI_API_KEY = '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'; // Thay bằng API key của bạn
